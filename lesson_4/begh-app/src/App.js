a29ce2a288a4bc3cd072059acac4f5e3db52584b
gnun <gnunua@gmail.com>
2018-03-16 23:05:41 +0400

    format

--- lesson_4/begh-app/src/App.js
+++ lesson_4/begh-app/src/App.js
@@ -3,40 +3,39 @@ import logo from "./logo.svg";
 import "./App.css";
 import {NameFormContolled, NameFormUnContolled} from "./NameForm";
 
-
 class A extends Component {
+  state = {text: "AAAA"};
 
-   state = {text : "AAAA"}
+  change = () => {
+    this.setState({text: "CCCC"});
+  };
 
-   change = ()=> {
-     this.setState({text: "CCCC"})
-   }
-
-   render(){
-     return <h1>{this.state.text}</h1>
-   }
+  render() {
+    return <h1>{this.state.text}</h1>;
+  }
 }
 
-function B(){
-  return <h1>BBBBBBBBBBBBB</h1>
-} 
+function B() {
+  return <h1>BBBBBBBBBBBBB</h1>;
+}
 
 class App extends Component {
   componentDidMount() {
     this.elRef.innerText = "innner Text";
-    this.el.change()
+    this.el.change();
   }
 
   render() {
     return (
-      <div onClick = { (e)=>{
-        console.log("----",e);
-        e.persist();
-        setTimeout( ()=> {
-          console.log(e.type);
-        }, 300 );
-       
-      }} className="App">
+      <div
+        onClick={e => {
+          console.log("----", e);
+          e.persist();
+          setTimeout(() => {
+            console.log(e.type);
+          }, 300);
+        }}
+        className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -48,19 +47,22 @@ class App extends Component {
           className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <NameFormContolled/>
-        <br/>
-        <br/>
-        <NameFormUnContolled/>
+        <NameFormContolled />
+        <br />
+        <br />
+        <NameFormUnContolled />
 
-        <A ref = { (el) => {
-           this.el = el;
-        }}/>
+        <A
+          ref={el => {
+            this.el = el;
+          }}
+        />
 
-        <B  ref = { (elB)=> {
-           console.log(elB);
-         }}  />
-
+        <B
+          ref={elB => {
+            console.log(elB);
+          }}
+        />
       </div>
     );
   }
